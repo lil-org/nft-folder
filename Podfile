f02fa2947f625fa99a58607c832e0827abe5a87b
ivan grachev <ivan@ivan.lol>
2023-11-17 22:57:39 +0300

    add trust wallet core

---
+++ Podfile
@@ -0,0 +1,7 @@
+inhibit_all_warnings!
+use_frameworks!
+
+target 'nft-folder' do
+  platform :osx, '13.0'
+  pod 'TrustWalletCore'
+end
---
+++ Pods/Target Support Files/Pods-nft-folder/Pods-nft-folder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_nft_folder : NSObject
+@end
+@implementation PodsDummy_Pods_nft_folder
+@end
