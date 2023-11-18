c5e6352d452274b775605732b50e761cf9fda9c8
ivan grachev <ivan@ivan.lol>
2023-11-18 18:39:29 +0300

    get nfts from 1inch api

--- shared/models/Asset.swift
+++ shared/models/Asset.swift
@@ -1,3 +1,59 @@
 // nft-folder-macos 2023 ethistanbul
 
 import Foundation
+
+struct Asset: Codable {
+    let id: Int
+    let tokenId: String
+    let permalink: String?
+    let animationOriginalUrl: String?
+    let description: String?
+    let name: String?
+    let imageOriginalUrl: String?
+    let externalLink: String?
+    let chainId: Int
+    let assetContract: AssetContract
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case tokenId = "token_id"
+        case animationOriginalUrl = "animation_original_url"
+        case description
+        case permalink
+        case name
+        case imageOriginalUrl = "image_original_url"
+        case externalLink = "external_link"
+        case chainId = "chainId"
+        case assetContract = "asset_contract"
+    }
+}
+
+struct AssetContract: Codable {
+    let address: String
+    let name: String
+    let description: String?
+    let createdDate: String
+    let externalLink: String?
+    let imageUrl: String?
+    let assetContractType: String?
+    let openseaVersion: String?
+    let nftVersion: String?
+    let payoutAddress: String?
+    let schemaName: String?
+    let symbol: String?
+
+    enum CodingKeys: String, CodingKey {
+        case address
+        case name
+        case description
+        case createdDate = "created_date"
+        case externalLink = "external_link"
+        case imageUrl = "image_url"
+        case assetContractType = "asset_contract_type"
+        case openseaVersion = "opensea_version"
+        case nftVersion = "nft_version"
+        case payoutAddress = "payout_address"
+        case schemaName = "schema_name"
+        case symbol
+    }
+}
--- shared/services/NFTService.swift
+++ shared/services/NFTService.swift
@@ -2,24 +2,50 @@
 
 import Foundation
 
+var uniqueIds = Set<Int>() // TODO: tmp
+
 struct NFTService {
+    
+    private struct Response: Decodable {
+        let assets: [Asset]
+    }
     
     private init() {}
     static let shared = NFTService()
     private let urlSession = URLSession.shared
+    private let apiKey = ""
     
-    func getNFTs(address: String) {
-        let apiKey = ""
-        let limit = 10
-        let offset = 0
+    func study(address: String, offset: Int = 0) {
+        print("will study offset \(offset)")
+        getNFTs(address: address, limit: 200, offset: offset) { assets in
+            print("\(assets.count) items at offset \(offset)")
+            
+            for asset in assets {
+                uniqueIds.insert(asset.id)
+            }
+            
+            print("uniqs count is \(uniqueIds.count)")
+            
+            if !assets.isEmpty {
+                study(address: address, offset: offset + assets.count)
+            }
+        }
+    }
+    
+    private func getNFTs(address: String, limit: Int, offset: Int, completion: @escaping ([Asset]) -> Void) {
+        print("will request offset \(offset)")
         let urlString = "https://api.1inch.dev/nft/v1/byaddress?chainIds=1&address=\(address)&limit=\(limit)&offset=\(offset)"
         guard let url = URL(string: urlString) else { return }
         var request = URLRequest(url: url)
         request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
         let task = urlSession.dataTask(with: request) { data, _, _ in
-            guard let data = data else { return } // TODO: retry or something
-            if let result = String(data: data, encoding: .utf8) {
-                print(result)
+            if let data = data, !data.isEmpty, let response = try? JSONDecoder().decode(Response.self, from: data) {
+                completion(response.assets)
+            } else {
+                print("it did not go well at offset \(offset)")
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+                    getNFTs(address: address, limit: limit, offset: offset, completion: completion)
+                }
             }
         }
         task.resume()
