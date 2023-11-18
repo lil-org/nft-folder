67f1d726a1203f5b1463de2321e1aac75f73a0f8
ivan grachev <ivan@ivan.lol>
2023-11-18 23:40:23 +0300

    better empty state for wallets list

--- views/WalletsListView.swift
+++ views/WalletsListView.swift
@@ -11,41 +11,47 @@ struct WalletsListView: View {
     private let wallets = WalletsService.shared.wallets
     
     var body: some View {
-        List {
-            Section(header: Text("wallets")) {
-                ForEach(wallets, id: \.self) { wallet in
-                    HStack {
-                        Circle()
-                            .frame(width: 30, height: 30)
-                            .overlay(
-                                Image(systemName: "person.fill") // TODO: avatar or blockies
-                                    .resizable()
-                                    .scaledToFit()
-                                    .clipShape(Circle())
-                            )
-                        Text(wallet.displayName)
-                    }
-                    .contentShape(Rectangle())
-                    .contextMenu {
-                        Button("remove", action: {
-                            // TODO: implement
-                        })
-                    }
-                }
-            }
-        }
-        
-        .toolbar {
-            ToolbarItemGroup(placement: .confirmationAction) {
-                Spacer()
-                Button(action: {
+        Group {
+            if wallets.isEmpty {
+                Button("add a wallet", action: {
                     showAddWalletPopup = true
-                }) {
-                    Image(systemName: "plus")
+                }).frame(maxWidth: .infinity, maxHeight: .infinity)
+            } else {
+                List {
+                    Section(header: Text("wallets")) {
+                        ForEach(wallets, id: \.self) { wallet in
+                            HStack {
+                                Circle()
+                                    .frame(width: 30, height: 30)
+                                    .overlay(
+                                        Image(systemName: "person.fill") // TODO: avatar or blockies
+                                            .resizable()
+                                            .scaledToFit()
+                                            .clipShape(Circle())
+                                    )
+                                Text(wallet.displayName)
+                            }
+                            .contentShape(Rectangle())
+                            .contextMenu {
+                                Button("remove", action: {
+                                    // TODO: implement
+                                })
+                            }
+                        }
+                    }
+                }
+                .toolbar {
+                    ToolbarItemGroup(placement: .confirmationAction) {
+                        Spacer()
+                        Button(action: {
+                            showAddWalletPopup = true
+                        }) {
+                            Image(systemName: "plus")
+                        }
+                    }
                 }
             }
-        }
-        .sheet(isPresented: $showAddWalletPopup) {
+        }.sheet(isPresented: $showAddWalletPopup) {
             VStack {
                 Text("add a wallet").fontWeight(.medium)
                 TextField("address or ens", text: $newWalletAddress)
@@ -55,11 +61,16 @@ struct WalletsListView: View {
                         showAddWalletPopup = false
                     })
                     Button("ok", action: {
-                        // TODO: add a wallet
+                        addWallet()
                     })
                 }
             }.frame(width: 320)
-            .padding()
+                .padding()
         }
     }
+    
+    func addWallet() {
+        // TODO: add a wallet
+    }
+    
 }
