a96e26bae4072ba50cf6fcc56a9b3d55607dacc9
ivan grachev <ivan@ivan.lol>
2023-11-19 05:04:50 +0300

    fallback to blockies as a folder icon when failed to get an avatar

--- helpers/FolderIcon.swift
+++ helpers/FolderIcon.swift
@@ -7,16 +7,29 @@ struct FolderIcon {
     static func set(for wallet: WatchOnlyWallet) {
         guard let avatar = wallet.avatar,
               let imageURL = URL(string: avatar),
-              let folderURL = URL.nftDirectory(wallet: wallet) else { return }
+              let folderURL = URL.nftDirectory(wallet: wallet) else {
+            setBlockies(for: wallet)
+            return
+        }
         let task = URLSession.shared.dataTask(with: imageURL) { data, response, error in
-            guard let data = data, error == nil else { return }
-            guard let image = NSImage(data: data) else { return }
+            guard let data = data, error == nil, let image = NSImage(data: data) else {
+                setBlockies(for: wallet)
+                return
+            }
             DispatchQueue.main.async {
                 NSWorkspace.shared.setIcon(image, forFile: folderURL.path, options: [])
             }
         }
         task.resume()
-        
+    }
+    
+    static private func setBlockies(for wallet: WatchOnlyWallet) {
+        DispatchQueue.main.async {
+            if let image = Blockies(seed: wallet.address.lowercased()).createImage(),
+               let folderURL = URL.nftDirectory(wallet: wallet) {
+                NSWorkspace.shared.setIcon(image, forFile: folderURL.path, options: [])
+            }
+        }
     }
     
 }
