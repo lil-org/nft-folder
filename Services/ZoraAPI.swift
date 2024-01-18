78c11cef200dcab17c7d58b950de44c0f7e7e9f3
ivan grachev <ivan@ivan.lol>
2024-01-18 18:28:42 +0300

    use smaller pagination limit for zora api

--- Services/ZoraAPI.swift
+++ Services/ZoraAPI.swift
@@ -29,7 +29,7 @@ struct ZoraAPI {
         let queryString = """
         {
           tokens(networks: [\(networksString)],
-                 pagination: {limit: 50\(endString)},
+                 pagination: {limit: 23\(endString)},
                  sort: {sortKey: MINTED, sortDirection: ASC},
                  where: \(whereString))
           {
