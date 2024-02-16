5ddbcfcdf8a2a96ad6134693ea5c3a50ee915378
ivan grachev <ivan@ivan.lol>
2024-02-16 18:13:13 +0300

    add DetailedTokenMetadata model

---
+++ Models/DetailedTokenMetadata.swift
@@ -0,0 +1,13 @@
+// ∅ nft-folder-macos 2024
+
+import Foundation
+
+// TODO: implement with more links, more data
+struct DetailedTokenMetadata: Codable {
+    
+    let name: String?
+    let collectionName: String?
+    let tokenUrl: String?
+    let selectedFileSourceUrl: String?
+    
+}
