6fcfa2c62d0ba146ea5f2fd754d6cc1bdf5dd53e
ivan grachev <ivan@ivan.lol>
2024-02-20 22:36:40 +0300

    use download tasks within FileDownloader

--- Models/DownloadFileTask.swift
+++ Models/DownloadFileTask.swift
@@ -10,8 +10,29 @@ struct DownloadFileTask {
     let minimalMetadata: MinimalTokenMetadata
     let detailedMetadata: DetailedTokenMetadata
     
-    var sourceIndex = 0 // TODO: not sure about this one
-    var currentURL: URL? // TODO: not sure about this one
+    private var sourceIndex = 0 // TODO: not sure about this one
+    
+    mutating func willTryAnotherSource() -> Bool {
+        if sourceIndex + 1 < detailedMetadata.probableDataOrUrls.count {
+            sourceIndex += 1
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    var currentDataOrURL: DataOrUrl? { // TODO: not sure about this one
+        guard detailedMetadata.probableDataOrUrls.count > sourceIndex else { return nil }
+        return detailedMetadata.probableDataOrUrls[sourceIndex]
+    }
+    
+    var currentURL: URL? { // TODO: not sure about this one
+        if case let .url(url) = currentDataOrURL {
+            return url
+        } else {
+            return nil
+        }
+    }
     
     init(destinationDirectory: URL, minimalMetadata: MinimalTokenMetadata, detailedMetadata: DetailedTokenMetadata) {
         self.destinationDirectory = destinationDirectory
