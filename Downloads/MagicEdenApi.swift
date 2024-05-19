834c557be1ce52168c76e2ba1458589a102f72e7
ivan grachev <ivan@ivan.lol>
2024-05-19 11:16:56 +0300

    stub magic eden api

---
+++ Downloads/MagicEdenApi.swift
@@ -0,0 +1,14 @@
+// ∅ nft-folder 2024
+
+import Foundation
+
+struct MagicEdenApi {
+    
+    private static let urlSession = URLSession.shared
+    private static let queue = DispatchQueue(label: "\(Bundle.hostBundleId).MagicEdenApi", qos: .default)
+    
+    static func get(owner: String) {
+        // TODO: implement
+    }
+    
+}
