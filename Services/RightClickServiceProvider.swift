8082d05941f9a251719b9c81115d1ef9add66ed1
ivan grachev <ivan@ivan.lol>
2024-05-31 22:51:34 +0300

    add ipfs upload sanity checks

--- Services/RightClickServiceProvider.swift
+++ Services/RightClickServiceProvider.swift
@@ -12,37 +12,25 @@ class RightClickServiceProvider: NSObject {
     
     private func processUrls(_ urls: [URL]) {
         let fileManager = FileManager.default
-        
-        var urlsToUpload = [URL]()
-        
         for url in urls {
             if url.isWithinNftDirectory {
                 Navigator.shared.show(filePath: url.path, on: .zora)
-                continue
             } else {
-                // TODO: filter out stuff that can't be uploaded, like big folders, etc.
-                urlsToUpload.append(url)
-            }
-            
-            if url.hasDirectoryPath, let children = try? fileManager.contentsOfDirectory(at: url, includingPropertiesForKeys: nil), !children.isEmpty {
-                // TODO: handle folders corresponding to existing nfts
-                processUrls(children)
-                return // TODO: make sure it does not go deep recursively
-            }
-            
-            if let attributes = try? fileManager.attributesOfItem(atPath: url.path),
-               let fileSize = attributes[.size] as? NSNumber {
-                if fileSize.uintValue > 10485760 {
-                    print("file is bigger than 10 mb")
-                    showErrorAlert(fileURL: url)
-                    // TODO: ask confirmation if there are big files
-                    return
+                if url.hasDirectoryPath {
+                    if let children = try? fileManager.contentsOfDirectory(at: url, includingPropertiesForKeys: nil), !children.isEmpty, urls.count == 1 {
+                        processUrls(children)
+                        return
+                    } else {
+                        continue
+                    }
+                } else if let attributes = try? fileManager.attributesOfItem(atPath: url.path), let fileSize = attributes[.size] as? NSNumber {
+                    guard fileSize.intValue < Int.defaultFileSizeLimit else {
+                        showErrorAlert(fileURL: url, withRetry: false)
+                        return
+                    }
+                    sendIt(fileURL: url)
                 }
             }
-        }
-        
-        for url in urlsToUpload {
-            sendIt(fileURL: url)
         }
     }
     
@@ -51,25 +39,29 @@ class RightClickServiceProvider: NSObject {
             if let cid = cid, let url = URL(string: "https://zora.co/create?image=ipfs://\(cid)") {
                 NSWorkspace.shared.open(url)
             } else {
-                self?.showErrorAlert(fileURL: fileURL)
+                self?.showErrorAlert(fileURL: fileURL, withRetry: true)
             }
         }
     }
     
-    func showErrorAlert(fileURL: URL) {
+    func showErrorAlert(fileURL: URL, withRetry: Bool) {
         let alert = NSAlert()
         alert.messageText = Strings.didNotUpload
         alert.informativeText = fileURL.lastPathComponent
         alert.alertStyle = .warning
-        alert.addButton(withTitle: Strings.retry)
-        alert.addButton(withTitle: Strings.cancel)
-        alert.buttons.last?.keyEquivalent = "\u{1b}"
-        let response = alert.runModal()
-        switch response {
-        case .alertFirstButtonReturn:
-            sendIt(fileURL: fileURL)
-        default:
-            break
+        if withRetry {
+            alert.addButton(withTitle: Strings.retry)
+            alert.addButton(withTitle: Strings.cancel)
+            alert.buttons.last?.keyEquivalent = "\u{1b}"
+            let response = alert.runModal()
+            switch response {
+            case .alertFirstButtonReturn:
+                sendIt(fileURL: fileURL)
+            default:
+                break
+            }
+        } else {
+            _ = alert.runModal()
         }
     }
     
