c6b7e34483f9cc34cac3e780b21ebc847936beff
ivan grachev <ivan@ivan.lol>
2024-06-02 22:31:35 +0300

    add synced folder model

---
+++ Models/SyncedFolder.swift
@@ -0,0 +1,19 @@
+// ∅ nft-folder 2024
+
+import Foundation
+
+struct SyncedFolder: Codable {
+    
+    let name: String?
+    let nfts: [NftInSyncedFolder]?
+    let childrenFolders: [SyncedFolder]?
+    
+}
+
+struct NftInSyncedFolder: Codable {
+
+    let chainId: String
+    let tokenId: String
+    let address: String
+    
+}
