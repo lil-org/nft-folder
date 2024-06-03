9f03095989a612981bec05184e958b44ab83ea16
ivan grachev <ivan@ivan.lol>
2024-06-03 13:01:56 +0300

    refactor ipfs upload

--- Services/IpfsUploader.swift
+++ Services/IpfsUploader.swift
@@ -4,18 +4,16 @@ import Foundation
 
 struct IpfsUploader {
     
-    static func upload(fileURL: URL, completion: @escaping (String?) -> Void) {
+    static func upload(name: String, mimeType: String, data: Data, completion: @escaping (String?) -> Void) {
         let boundary = "----WebKitFormBoundaryn3JBuHDuzWcHa9BR"
         var request = URLRequest(url: URL(string: "https://ipfs-uploader.zora.co/api/v0/add?stream-channels=true&cid-version=1&progress=false")!)
         request.httpMethod = "POST"
         request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-
         var body = Data()
         body.append(Data("--\(boundary)\r\n".utf8))
-        body.append(Data("Content-Disposition: form-data; name=\"file\"; filename=\"\(fileURL.lastPathComponent)\"\r\n".utf8))
-        body.append(Data("Content-Type: \(fileURL.mimeType)\r\n\r\n".utf8))
-        guard let fileData = try? Data(contentsOf: fileURL) else { return }
-        body.append(fileData)
+        body.append(Data("Content-Disposition: form-data; name=\"file\"; filename=\"\(name)\"\r\n".utf8))
+        body.append(Data("Content-Type: \(mimeType)\r\n\r\n".utf8))
+        body.append(data)
         body.append(Data("\r\n--\(boundary)--\r\n".utf8))
         request.httpBody = body
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
@@ -26,6 +24,11 @@ struct IpfsUploader {
             }
         }
         task.resume()
+    }
+    
+    static func upload(fileURL: URL, completion: @escaping (String?) -> Void) {
+        guard let fileData = try? Data(contentsOf: fileURL) else { return }
+        upload(name: fileURL.lastPathComponent, mimeType: fileURL.mimeType, data: fileData, completion: completion)
     }
     
 }
