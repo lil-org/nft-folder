115b6183c9582d00b111f3dc824582cf93bc8bb8
ivan grachev <ivan@ivan.lol>
2024-06-03 12:48:29 +0300

    ask to confirm saving folders onchain

--- Services/FolderSyncService.swift
+++ Services/FolderSyncService.swift
@@ -5,14 +5,28 @@ import Cocoa
 struct FolderSyncService {
     
     static func pushCustomFolders(wallet: WatchOnlyWallet) {
-        // TODO: implement
-        
-        // TODO: check if folders were changed
-        // TODO: confirm it's yours wallet
-        // TODO: upload to ipfs
-        // TODO: redirect to hash uploader page
-        if let galleryURL = NftGallery.opensea.url(wallet: wallet) {
-            DispatchQueue.main.async { NSWorkspace.shared.open(galleryURL) }
+        showConfirmationAlert(wallet: wallet)
+    }
+    
+    private static func showConfirmationAlert(wallet: WatchOnlyWallet) {
+        let alert = NSAlert()
+        alert.messageText = Strings.pushCustomFolders + "?"
+        alert.informativeText = wallet.folderDisplayName
+        alert.alertStyle = .informational
+        alert.addButton(withTitle: Strings.ok)
+        alert.addButton(withTitle: Strings.cancel)
+        alert.buttons.last?.keyEquivalent = "\u{1b}"
+        let response = alert.runModal()
+        switch response {
+        case .alertFirstButtonReturn:
+            // TODO: check if folders were changed
+            // TODO: upload to ipfs
+            // TODO: redirect to hash uploader page
+            if let galleryURL = NftGallery.opensea.url(wallet: wallet) {
+                DispatchQueue.main.async { NSWorkspace.shared.open(galleryURL) }
+            }
+        default:
+            break
         }
     }
     
