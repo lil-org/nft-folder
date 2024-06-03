8e2ecb8cdbe0e22eb6c9abcfb7662eeab84d3b51
ivan grachev <ivan@ivan.lol>
2024-06-03 13:24:03 +0300

    stub ipfs upload and redirect for onchain folders push

--- Services/FolderSyncService.swift
+++ Services/FolderSyncService.swift
@@ -19,15 +19,31 @@ struct FolderSyncService {
         let response = alert.runModal()
         switch response {
         case .alertFirstButtonReturn:
-            // TODO: check if folders were changed
-            // TODO: upload to ipfs
-            // TODO: redirect to hash uploader page
-            if let galleryURL = NftGallery.opensea.url(wallet: wallet) {
-                DispatchQueue.main.async { NSWorkspace.shared.open(galleryURL) }
-            }
+            uploadFoldersToIpfsAndSaveOnchain(wallet: wallet)
         default:
             break
         }
+    }
+    
+    private static func uploadFoldersToIpfsAndSaveOnchain(wallet: WatchOnlyWallet) {
+        // TODO: check if folders were changed
+        // TODO: create file properly
+        guard let fileData = wallet.address.data(using: .utf8) else { return }
+        IpfsUploader.upload(name: wallet.address, mimeType: "application/json", data: fileData) { cid in
+            if let cid = cid, let url = URL(string: "https://zora.co/create?image=ipfs://\(cid)") {
+                // TODO: open EAS url
+                NSWorkspace.shared.open(url)
+            } else {
+                showErrorAlert()
+            }
+        }
+    }
+    
+    private static func showErrorAlert() {
+        let alert = NSAlert()
+        alert.messageText = Strings.somethingWentWrong
+        alert.alertStyle = .warning
+        _ = alert.runModal()
     }
     
 }
