3b0b1d05cbfc72ca058a090d47acf87332f55c85
ivan grachev <ivan@ivan.lol>
2024-06-12 20:36:48 +0300

    stub app store metadata translation

--- translate/main.swift
+++ translate/main.swift
@@ -2,5 +2,20 @@
 
 import Foundation
 
-print("yo")
+enum MetadataKind: String {
+    case description
+    case keywords
+    case name
+    case subtitle
+    case promotionalText = "promotional_text"
+    case releaseNotes = "release_notes"
+}
 
+translateAppStoreMetadata()
+
+func translateAppStoreMetadata() {
+    print("yo")
+    // TODO: go trough all locales
+    // TODO: use en and ru versions as a reference
+    // TODO: check if there were changes since the last translation run
+}
