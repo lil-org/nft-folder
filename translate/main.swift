0db033f198a9e532178e14629f8cb60551296ebd
ivan grachev <ivan@ivan.lol>
2024-06-12 21:06:13 +0300

    enum supported languages

--- translate/main.swift
+++ translate/main.swift
@@ -11,6 +11,43 @@ enum MetadataKind: String {
     case releaseNotes = "release_notes"
 }
 
+enum Language: String, CaseIterable {
+    case english = "en"
+    case arabic = "ar"
+    case catalan = "ca"
+    case chinese = "zh-Hans"
+    case croatian = "hr"
+    case czech = "cs"
+    case danish = "da"
+    case dutch = "nl"
+    case finnish = "fi"
+    case french = "fr"
+    case german = "de"
+    case greek = "el"
+    case hebrew = "he"
+    case hindi = "hi"
+    case hungarian = "hu"
+    case indonesian = "id"
+    case italian = "it"
+    case japanese = "ja"
+    case korean = "ko"
+    case malay = "ms"
+    case norwegian = "nb"
+    case polish = "pl"
+    case portugeseBrazil = "pt-BR"
+    case portugese = "pt-PT"
+    case romanian = "ro"
+    case russian = "ru"
+    case slovak = "sk"
+    case spanish = "es"
+    case spanishLatinAmerica = "es-419"
+    case swedish = "sv"
+    case thai = "th"
+    case turkish = "tr"
+    case ukrainian = "uk"
+    case vietnamese = "vi"
+}
+
 translateAppStoreMetadata()
 
 func translateAppStoreMetadata() {
