048a15db9193663d8e075c354c012759d11e192f
ivan grachev <ivan@ivan.lol>
2024-06-13 18:24:08 +0300

    stub metadata translation

--- translate/main.swift
+++ translate/main.swift
@@ -2,31 +2,34 @@
 
 import Foundation
 
+let semaphore = DispatchSemaphore(value: 0)
+let queue = DispatchQueue(label: UUID().uuidString, qos: .default)
 let projectDir = FileManager.default.currentDirectoryPath
 let metadataDir = "\(projectDir)/fastlane/metadata/"
 
 translateAppStoreMetadata()
 
 func translateAppStoreMetadata() {
+    var translationTasksCount = MetadataKind.allCases.filter { $0.toTranslate }.count * (Language.allCases.count - 2)
     for metadataKind in MetadataKind.allCases {
-        let englishValue = read(metadataKind: metadataKind, language: .english)
-        let russianValue = read(metadataKind: metadataKind, language: .russian)
+        let englishText = read(metadataKind: metadataKind, language: .english)
+        let russianText = read(metadataKind: metadataKind, language: .russian)
         for language in Language.allCases where language != .english {
             if metadataKind.toTranslate {
                 guard language != .russian else { continue }
-                // TODO: check if there were changes since the last translation run
-                // TODO: implement translation using ru and eng
-                // A description of your app, detailing features and functionality.
-                // Separate keywords with an English comma, Chinese comma, or a mix of both.
-                // max 100 chars for keywords
-                // Describe what's new in this version of your app, such as new features, improvements, and bug fixes.
-                let translation = englishValue
-                write(translation, metadataKind: metadataKind, language: language)
+                translate(metadataKind: metadataKind, englishText: englishText, russianText: russianText) { translation in
+                    write(translation, metadataKind: metadataKind, language: language)
+                    translationTasksCount -= 1
+                    if translationTasksCount == 0 {
+                        semaphore.signal()
+                    }
+                }
             } else {
-                write(englishValue, metadataKind: metadataKind, language: language)
+                write(englishText, metadataKind: metadataKind, language: language)
             }
         }
     }
+    semaphore.wait()
 }
 
 func read(metadataKind: MetadataKind, language: Language) -> String {
@@ -45,3 +48,16 @@ func write(_ newValue: String, metadataKind: MetadataKind, language: Language) {
 func url(metadataKind: MetadataKind, language: Language) -> URL {
     return URL(fileURLWithPath: metadataDir + "\(language.metadataLocalizationKey)/\(metadataKind.fileName).txt")
 }
+
+func translate(metadataKind: MetadataKind, englishText: String, russianText: String, completion: @escaping (String) -> Void) {
+    completion(englishText)
+    // TODO: check if there were changes since the last translation run
+    // TODO: complete with a current version if there is no need to translate again
+    // TODO: implement translation using ru and eng
+    // A description of your app, detailing features and functionality.
+    // Separate keywords with an English comma, Chinese comma, or a mix of both.
+    // max 100 chars for keywords
+    // Describe what's new in this version of your app, such as new features, improvements, and bug fixes.
+}
+
+print("🟢 all done")
