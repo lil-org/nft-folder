9c5a52d7d709db442026f1b19adb4ec6edcdb4c2
ivan grachev <ivan@ivan.lol>
2024-06-19 18:53:39 +0300

    keep avatars in memory

--- Services/AvatarService.swift
+++ Services/AvatarService.swift
@@ -2,9 +2,12 @@
 
 import Cocoa
 
+// TODO: clean in-memory cache if there was no opened window for a while
+
 struct AvatarService {
     
     private static var attemptsCountDict = [String: Int]()
+    private static var dict = [URL: NSImage]()
     
     static func getAvatar(wallet: WatchOnlyWallet, completion: @escaping (NSImage) -> Void) {
         guard let urlString = wallet.avatar,
@@ -12,8 +15,14 @@ struct AvatarService {
               let fileURL = URL.avatarOnDisk(wallet: wallet),
               let folderURL = URL.nftDirectory(wallet: wallet, createIfDoesNotExist: false) else { return }
         
+        if let image = dict[fileURL] {
+            completion(image)
+            return
+        }
+        
         if let diskCachedData = try? Data(contentsOf: fileURL), let image = NSImage(data: diskCachedData) {
             completion(image)
+            dict[fileURL] = image
             return
         }
         
@@ -29,9 +38,13 @@ struct AvatarService {
         
         URLSession.shared.dataTask(with: url) { data, _, error in
             guard let data = data, error == nil, let image = NSImage(data: data) else { return }
-            completion(image)
+            // TODO: make it smaller if needed, do not store an image too big
+            DispatchQueue.main.async {
+                completion(image)
+                dict[fileURL] = image
+                NSWorkspace.shared.setIcon(image, forFile: folderURL.path, options: [])
+            }
             try? data.write(to: fileURL, options: .atomic)
-            DispatchQueue.main.async { NSWorkspace.shared.setIcon(image, forFile: folderURL.path, options: []) }
         }.resume()
     }
     
