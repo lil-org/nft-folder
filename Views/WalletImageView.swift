607060d5f8e77f576871373109ed42253dc602a6
ivan grachev <ivan@ivan.lol>
2024-06-20 17:20:47 +0300

    clean up random gradient

--- Views/WalletImageView.swift
+++ Views/WalletImageView.swift
@@ -57,23 +57,21 @@ struct RandomGradientShape: View {
     var body: some View {
         let colors = generateColors(from: address)
         let gradient = LinearGradient(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
-        
-        return Rectangle()
-            .fill(gradient)
+        return Rectangle().fill(gradient)
     }
     
     func generateColors(from address: String) -> [Color] {
         let hash = simpleHash(address)
         return [
-            Color(red: Double((hash >> 0) & 0xFF) / 255.0, green: Double((hash >> 8) & 0xFF) / 255.0, blue: Double((hash >> 16) & 0xFF) / 255.0),
-            Color(red: Double((hash >> 24) & 0xFF) / 255.0, green: Double((hash >> 32) & 0xFF) / 255.0, blue: Double((hash >> 40) & 0xFF) / 255.0)
+            Color(red: Double((hash & 0xFF)) / 255.0, green: Double((hash >> 8 & 0xFF)) / 255.0, blue: Double((hash >> 16 & 0xFF)) / 255.0),
+            Color(red: Double((hash >> 24 & 0xFF)) / 255.0, green: Double((hash >> 32 & 0xFF)) / 255.0, blue: Double((hash >> 40 & 0xFF)) / 255.0)
         ]
     }
     
     func simpleHash(_ input: String) -> UInt64 {
         var hash: UInt64 = 0
         for char in input.utf8 {
-            hash = 31 &* hash &+ UInt64(char)
+            hash = (hash &* 31 &+ UInt64(char)) & UInt64.max
         }
         return hash
     }
