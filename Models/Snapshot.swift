6bed5994f56a6fb11afff06de59ed46321e82c24
ivan grachev <ivan@ivan.lol>
2024-06-21 17:23:30 +0300

    do not use comment value when comparing tokens in synced folders

--- Models/Snapshot.swift
+++ Models/Snapshot.swift
@@ -17,11 +17,25 @@ struct Folder: Codable {
     let cover: String?
 }
 
-struct Token: Codable, Hashable {
+struct Token: Codable {
     let id: String
     let address: String
     let chainId: String
     let comment: String?
+}
+
+extension Token: Hashable {
+    static func == (lhs: Token, rhs: Token) -> Bool {
+        return lhs.id == rhs.id &&
+               lhs.address == rhs.address &&
+               lhs.chainId == rhs.chainId
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(address)
+        hasher.combine(chainId)
+    }
 }
 
 struct RemainingFoldersForTokens: Codable {
