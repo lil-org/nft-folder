f6e0dfcd512f0e4e846326d0b53983472d3e7afe
ivan grachev <ivan@ivan.lol>
2024-06-26 13:52:14 +0300

    use consistent name style for suggested items

--- Models/SuggestedItem.swift
+++ Models/SuggestedItem.swift
@@ -14,7 +14,7 @@ struct SuggestedItem: Identifiable, Hashable {
         SuggestedItem(name: "Pudgy Penguins", address: "0xbd3531da5cf5857e7cfaa92426877b022e612cf8"),
         SuggestedItem(name: "Creature World", address: "0xc92ceddfb8dd984a89fb494c376f9a48b999aafc"),
         SuggestedItem(name: "Chromie Squiggle", address: "0x059edd72cd353df5106d2b9cc5ab83a52287ac3a"),
-        SuggestedItem(name: "PEACEFUL GROUPIES", address: "0x4f89cd0cae1e54d98db6a80150a824a533502eea"),
+        SuggestedItem(name: "Peaceful Groupies", address: "0x4f89cd0cae1e54d98db6a80150a824a533502eea"),
         SuggestedItem(name: "Fidenza", address: "0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270"),
         SuggestedItem(name: "Very Internet Person", address: "0xfed18c828277e3bd8610f9bae432e65a651706f7")
     ]
