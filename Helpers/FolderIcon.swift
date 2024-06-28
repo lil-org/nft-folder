2edd2be1d4a5952a7f4c259d9414ec6385ac613f
ivan grachev <ivan@ivan.lol>
2024-06-29 00:16:41 +0300

    add todo

--- Helpers/FolderIcon.swift
+++ Helpers/FolderIcon.swift
@@ -5,6 +5,7 @@ import Cocoa
 struct FolderIcon {
     
     static func set(for wallet: WatchOnlyWallet) {
+        // TODO: make sure it writes on disk, not just returns cached value
         AvatarService.getAvatar(wallet: wallet) { _ in }
     }
     
