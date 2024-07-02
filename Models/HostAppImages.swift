97668247d3eb2263684f29d9af7c223ce8cb4119
ivan grachev <ivan@ivan.lol>
2024-07-02 20:24:06 +0300

    update app icon

--- Models/HostAppImages.swift
+++ Models/HostAppImages.swift
@@ -9,7 +9,6 @@ extension Images {
     static let gearshape = Image(systemName: "gearshape")
     static let quit = Image(systemName: "xmark.circle")
     static let extend = Image(systemName: "arrow.down.backward.and.arrow.up.forward.circle")
-    static let logo = Image("folderLogo")
     static let pause = Image(systemName: "pause.fill")
     static let sync = Image(systemName: "arrow.clockwise")
     
