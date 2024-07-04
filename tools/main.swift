ca6e49ad5dae109795cfc53643a9efad6b0f4cd9
ivan grachev <ivan@ivan.lol>
2024-07-04 18:45:37 +0300

    add autoscope

--- tools/main.swift
+++ tools/main.swift
@@ -3,11 +3,13 @@
 import Cocoa
 
 let fresh: [(String, Chain)] = [
-    ("0x5a0121a0a21232ec0d024dab9017314509026480", .ethereum)
+    ("0xe7ed54673455193f1d4448c88c6544fd93a237c0", .ethereum)
 ]
 
-prepareForSelection(input: fresh)
+//prepareForSelection(input: fresh)
+
 //bundleSelected()
+
 //rebundleImages(onlyMissing: true, useCollectionImage: true)
 
 print("🟢 all done")
