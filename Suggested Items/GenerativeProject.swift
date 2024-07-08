74a06305df03c3121f8c022b6fe78ca342fe117e
ivan grachev <ivan@ivan.lol>
2024-07-08 23:58:52 +0300

    bundle generative projects

---
+++ Suggested Items/GenerativeProject.swift
@@ -0,0 +1,28 @@
+// ∅ nft-folder 2024
+
+import Foundation
+
+struct GenerativeProject: Codable {
+    
+    var id: String {
+        return contractAddress + projectId
+    }
+    
+    let contractAddress: String
+    let projectId: String
+    let tokens: [Token]
+    let script: String
+    let kind: Kind
+    let instructions: String?
+    
+    enum Kind: String, Codable {
+        case svg, js, p5js, custom
+    }
+    
+    struct Token: Codable {
+        
+        let id: String
+        let hash: String
+        
+    }
+}
