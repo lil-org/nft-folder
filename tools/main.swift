f116c2a48eb0a26e8c750fbf09d89f3a1d6f161b
ivan grachev <ivan@ivan.lol>
2024-07-09 00:13:39 +0300

    stub generating html

--- tools/main.swift
+++ tools/main.swift
@@ -14,4 +14,59 @@ let fresh =
 
 //removeBundledItem(name: "")
 
+var generativeProjects = [GenerativeProject]() // TODO: read from bundle
+
+for p in generativeProjects {
+    let html = createRandomTokenHtml(project: p)
+    // TODO: save html
+}
+
 print("🟢 all done")
+
+func createRandomTokenHtml(project: GenerativeProject) -> String {
+    // TODO: fill in template data correctly
+    let template =
+"""
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <meta charset="utf-8"/>
+    <script>
+        // TODO: insert p5js here if needed
+    </script>
+    <script>
+    let tokenData = {
+        "tokenId": "114000424",
+        "hash": "0x710e58f1a85051eb392ff157f7118f46cb6313708e2202644c9b3e3278e8a517"
+    }
+    </script>
+    <script>
+        // TODO: insert project script here
+    </script>
+    <style type="text/css">
+    html {
+        height: 100%;
+    }
+
+    body {
+        min-height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    canvas {
+        padding: 0;
+        margin: auto;
+        display: block;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+    </style>
+</head>
+</html>
+"""
+    return template
+}
