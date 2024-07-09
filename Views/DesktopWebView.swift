874ebd4b870c4ccc8c9ef895658449774538b30e
ivan grachev <ivan@ivan.lol>
2024-07-09 19:15:03 +0300

    tune desktop webview references

--- Views/DesktopWebView.swift
+++ Views/DesktopWebView.swift
@@ -6,31 +6,33 @@ import WebKit
 struct DesktopWebView: NSViewRepresentable {
     
     var htmlContent: String
-    @State private var webView: WKWebView?
     
     func makeNSView(context: Context) -> NSView {
         let containerView = NSView()
-        DispatchQueue.main.async {
+        DispatchQueue.main.async { [weak containerView] in
             let webConfiguration = WKWebViewConfiguration()
             webConfiguration.suppressesIncrementalRendering = true
             let wkWebView = WKWebView(frame: .zero, configuration: webConfiguration)
             wkWebView.translatesAutoresizingMaskIntoConstraints = false
-            containerView.addSubview(wkWebView)
-            NSLayoutConstraint.activate([
-                wkWebView.topAnchor.constraint(equalTo: containerView.topAnchor),
-                wkWebView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
-                wkWebView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
-                wkWebView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
-            ])
-            wkWebView.loadHTMLString(htmlContent, baseURL: nil)
-            self.webView = wkWebView
+            if let containerView = containerView {
+                containerView.addSubview(wkWebView)
+                NSLayoutConstraint.activate([
+                    wkWebView.topAnchor.constraint(equalTo: containerView.topAnchor),
+                    wkWebView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+                    wkWebView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+                    wkWebView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+                ])
+                wkWebView.loadHTMLString(htmlContent, baseURL: nil)
+            }
         }
         return containerView
     }
     
     func updateNSView(_ nsView: NSView, context: Context) {
-        if let webView = webView {
-            webView.loadHTMLString(htmlContent, baseURL: nil)
+        DispatchQueue.main.async {
+            if let wkWebView = nsView.subviews.first as? WKWebView {
+                wkWebView.loadHTMLString(htmlContent, baseURL: nil)
+            }
         }
     }
     
