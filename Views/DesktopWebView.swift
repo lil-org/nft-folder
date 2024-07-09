b64f95f73bb7403af107df0e22b2722b0370047a
ivan grachev <ivan@ivan.lol>
2024-07-09 19:03:41 +0300

    fix webview first appearance lag

--- Views/DesktopWebView.swift
+++ Views/DesktopWebView.swift
@@ -6,13 +6,32 @@ import WebKit
 struct DesktopWebView: NSViewRepresentable {
     
     var htmlContent: String
+    @State private var webView: WKWebView?
     
-    func makeNSView(context: Context) -> WKWebView {
-        WKWebView()
+    func makeNSView(context: Context) -> NSView {
+        let containerView = NSView()
+        DispatchQueue.main.async {
+            let webConfiguration = WKWebViewConfiguration()
+            webConfiguration.suppressesIncrementalRendering = true
+            let wkWebView = WKWebView(frame: .zero, configuration: webConfiguration)
+            wkWebView.translatesAutoresizingMaskIntoConstraints = false
+            containerView.addSubview(wkWebView)
+            NSLayoutConstraint.activate([
+                wkWebView.topAnchor.constraint(equalTo: containerView.topAnchor),
+                wkWebView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+                wkWebView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+                wkWebView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+            ])
+            wkWebView.loadHTMLString(htmlContent, baseURL: nil)
+            self.webView = wkWebView
+        }
+        return containerView
     }
     
-    func updateNSView(_ nsView: WKWebView, context: Context) {
-        nsView.loadHTMLString(htmlContent, baseURL: nil)
+    func updateNSView(_ nsView: NSView, context: Context) {
+        if let webView = webView {
+            webView.loadHTMLString(htmlContent, baseURL: nil)
+        }
     }
     
 }
