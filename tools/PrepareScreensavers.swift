eac5dc03d7b33023ef7e7b9a8dbb0f1688651c84
ivan grachev <ivan@ivan.lol>
2024-07-12 23:23:13 +0300

    prepare data for screensavers

--- tools/PrepareScreensavers.swift
+++ tools/PrepareScreensavers.swift
@@ -1,16 +1,23 @@
 // ∅ nft-folder 2024
 
-import Foundation
+import Cocoa
 
 func setupForScreenSaverBuild(_ number: Int) {
-    // TODO: implement
-    let jsonNames = try! FileManager.default.contentsOfDirectory(atPath: dir + "/Suggested Items/Suggested.bundle/Generative/")
-    var items = [SuggestedItem]()
-    for jsonName in jsonNames {
-        let data = try! Data(contentsOf: URL(filePath: dir + "/Suggested Items/Suggested.bundle/Generative/" + jsonName))
-        let generativeProject = try! JSONDecoder().decode(GenerativeProject.self, from: data)
-        let item = bundledSuggestedItems.first(where: { $0.id == generativeProject.id })!
-        items.append(item)
-        print(item.name)
-    }
+    let jsonNames = try! FileManager.default.contentsOfDirectory(atPath: dir + "/Suggested Items/Suggested.bundle/Generative/").sorted()
+    
+    let jsonName = jsonNames[number]
+    let data = try! Data(contentsOf: URL(filePath: dir + "/Suggested Items/Suggested.bundle/Generative/" + jsonName))
+    let generativeProject = try! JSONDecoder().decode(GenerativeProject.self, from: data)
+    let item = bundledSuggestedItems.first(where: { $0.id == generativeProject.id })!
+    
+    NSPasteboard.general.clearContents()
+    NSPasteboard.general.setString(item.name, forType: .string)
+    
+    let lib = ScriptType(rawValue: generativeProject.kind.rawValue)?.libScript
+    let script = generativeProject.script
+    let imageData = try! Data(contentsOf: URL(filePath: wipPath + "/previews/\(item.id)"))
+    
+    // TODO: put script in there
+    // TODO: put library in there
+    // TODO: put thumbnail image in there
 }
