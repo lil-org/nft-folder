48087a9697f12aa9db6ec6156a0ebd7f58a14b28
ivan grachev <ivan@ivan.lol>
2024-07-24 13:07:44 +0300

    add vision target

---
+++ nft-folder-vision/nft_folder_visionApp.swift
@@ -0,0 +1,12 @@
+// ∅ nft-folder 2024
+
+import SwiftUI
+
+@main
+struct nft_folder_visionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
