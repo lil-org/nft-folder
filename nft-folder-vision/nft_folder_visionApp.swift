a561be7c4978837badd1dff66ea376abd4748a85
ivan grachev <ivan@ivan.lol>
2024-07-24 16:55:12 +0300

    stub vision windows

--- nft-folder-vision/nft_folder_visionApp.swift
+++ nft-folder-vision/nft_folder_visionApp.swift
@@ -5,8 +5,40 @@ import SwiftUI
 @main
 struct nft_folder_visionApp: App {
     var body: some Scene {
-        WindowGroup {
+        WindowGroup(id: "suggested-items") {
             ContentView()
         }
+        
+        WindowGroup(id: "player") {
+            PlayerView()
+        }
     }
+}
+
+struct ContentView: View {
+    
+    @Environment(\.openWindow) private var openWindow
+    
+    var body: some View {
+        VStack {
+            Button("Play", action: {
+                play()
+            })
+        }
+        .padding()
+    }
+    
+    private func play() {
+        openWindow(id: "player")
+    }
+    
+}
+
+
+struct PlayerView: View {
+    
+    var body: some View {
+        Text("playing...").font(.headline)
+    }
+    
 }
