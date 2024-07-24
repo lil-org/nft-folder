eda09b067cd1486caf722abd3ef627a461c98e81
ivan grachev <ivan@ivan.lol>
2024-07-24 17:07:59 +0300

    stub tvos target

---
+++ nft-folder-tvos/ContentView.swift
@@ -0,0 +1,12 @@
+// ∅ nft-folder 2024
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
