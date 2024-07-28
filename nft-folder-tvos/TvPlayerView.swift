00818a2f9adcfd4d7d0517d9b9b18c56cd631a1f
ivan grachev <ivan@ivan.lol>
2024-07-28 22:54:59 +0300

    show info on tv

--- nft-folder-tvos/TvPlayerView.swift
+++ nft-folder-tvos/TvPlayerView.swift
@@ -13,44 +13,46 @@ struct TvPlayerView: View {
     }
     
     var body: some View {
-        GeneratedTokenView(contentString: playerModel.currentToken.html).edgesIgnoringSafeArea(.all)
-            .onAppear() {
-                if !playerModel.history.isEmpty {
-                    playerModel.goForward()
+        ZStack(alignment: .topTrailing) {
+            GeneratedTokenView(contentString: playerModel.currentToken.html).edgesIgnoringSafeArea(.all)
+                .onAppear() {
+                    if !playerModel.history.isEmpty {
+                        playerModel.goForward()
+                    }
                 }
-            }
-            .focusable()
-            .onMoveCommand { direction in
-                switch direction {
-                case .left:
-                    DispatchQueue.main.async { playerModel.goBack() }
-                case .right:
-                    DispatchQueue.main.async { playerModel.goForward() }
-                case .up:
-                    DispatchQueue.main.async { playerModel.goBack() }
-                case .down:
-                    DispatchQueue.main.async { playerModel.changeCollection() }
-                default:
-                    break
+                .focusable()
+                .onMoveCommand { direction in
+                    switch direction {
+                    case .left:
+                        DispatchQueue.main.async { playerModel.goBack() }
+                    case .right:
+                        DispatchQueue.main.async { playerModel.goForward() }
+                    case .up:
+                        DispatchQueue.main.async { playerModel.goBack() }
+                    case .down:
+                        DispatchQueue.main.async { playerModel.changeCollection() }
+                    default:
+                        break
+                    }
                 }
+                .onPlayPauseCommand {
+                    showInfoPopover.toggle()
+                }
+            
+            if showInfoPopover {
+                infoPopoverView()
+                    .background(Color.black.opacity(0.7))
+                    .cornerRadius(15)
+                    .padding()
             }
-            .onPlayPauseCommand {
-                showInfoPopover.toggle()
-            }
-        
-            // .overlay(infoPopoverView())
+        }
     }
     
     private func infoPopoverView() -> some View {
         VStack(alignment: .leading, spacing: 10) {
             Text(playerModel.currentToken.displayName)
-            Divider()
-            if let instructions = playerModel.currentToken.instructions {
-                Divider()
-                Text(instructions).font(.body)
-            }
         }
-        .padding().frame(width: 230)
+        .padding().frame(minWidth: 250)
     }
     
 }
