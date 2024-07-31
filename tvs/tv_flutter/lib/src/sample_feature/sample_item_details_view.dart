600c778f15ec5715dd3e9fc6aa9c2e401ccf2b94
ivan grachev <ivan@ivan.lol>
2024-07-31 16:06:39 +0300

    show random color html in tv player

--- tvs/tv_flutter/lib/src/sample_feature/sample_item_details_view.dart
+++ tvs/tv_flutter/lib/src/sample_feature/sample_item_details_view.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:convert';
+import 'dart:math';
 
 class SampleItemDetailsView extends StatelessWidget {
   const SampleItemDetailsView({super.key});
 
   static const routeName = '/sample_item';
 
+  String _getRandomColor() {
+    final random = Random();
+    return '#${random.nextInt(0xFFFFFF).toRadixString(16).padLeft(6, '0')}';
+  }
+
   @override
   Widget build(BuildContext context) {
+    final randomColor = _getRandomColor();
+    final htmlContent = '''
+      <html>
+        <body style="background-color: $randomColor;">
+        </body>
+      </html>
+    ''';
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('item details'),
+        title: const Text('webview'),
       ),
-      body: const Center(
-        child: Text('more info'),
+      body: WebViewWidget(
+        controller: WebViewController()
+          ..loadRequest(
+            Uri.dataFromString(
+              htmlContent,
+              mimeType: 'text/html',
+              encoding: utf8,
+            ),
+          )
+          ..setJavaScriptMode(JavaScriptMode.unrestricted),
       ),
     );
   }
