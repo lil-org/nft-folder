5c631820dac791680d421790d9ea0a54bc66c705
ivan grachev <ivan@ivan.lol>
2024-08-02 15:07:21 +0300

    generate on google tv

--- tvs/tv_flutter/lib/src/player/item_view.dart
+++ tvs/tv_flutter/lib/src/player/item_view.dart
@@ -72,37 +72,247 @@ class SampleItemDetailsView extends StatelessWidget {
     final Script script = Script.fromJson(json.decode(scriptJsonString));
     final String lib = (script.kind == "js" || script.kind == "svg") ? "" : await rootBundle.loadString('assets/items/libs/${script.kind}.js');
 
-    final random = Random();
-    final r = random.nextInt(256);
-    final g = random.nextInt(256);
-    final b = random.nextInt(256);
+    final token = bundledTokens.items.first;
+    final hash = token.hash;
+    final tokenid = token.id;
 
-    return '''
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>
-          body { 
-            background-color: rgb($r, $g, $b);
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: Arial, sans-serif;
-          }
-          h1 {
-            color: white;
-          }
-        </style>
-      </head>
-      <body>
-        <h1>${item.name} ${script.name} ${script.kind} ${bundledTokens.items.length} $lib</h1>
-      </body>
-      </html>
-    ''';
+    String tokenData;
+    if (script.address == "0x059edd72cd353df5106d2b9cc5ab83a52287ac3a") {
+      tokenData = 'let tokenData = {"tokenId": "$tokenid", "hashes": ["$hash"]}';
+    } else {
+      tokenData = 'let tokenData = {"tokenId": "$tokenid", "hash": "$hash"}';
+    }
+
+    const viewport = '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>';
+
+    String html;
+    switch (script.kind) {
+      case 'svg':
+        html = '''
+          <html>
+          <head>
+            $viewport
+            <meta charset="utf-8">
+            <style type="text/css">
+              body {
+                min-height: 100%;
+                margin: 0;
+                padding: 0;
+              }
+              svg {
+                padding: 0;
+                margin: auto;
+                display: block;
+                position: absolute;
+                top: 0;
+                bottom: 0;
+                left: 0;
+                right: 0;
+              }
+            </style>
+          </head>
+          <body></body>
+          <script>$tokenData</script>
+          <script>${script.value}</script>
+          </html>
+        ''';
+        break;
+      case 'js':
+        html = '''
+          <html>
+          <head>
+            $viewport
+            <meta charset="utf-8">
+            <script>$tokenData</script>
+            <style type="text/css">
+              body {
+                margin: 0;
+                padding: 0;
+              }
+              canvas {
+                padding: 0;
+                margin: auto;
+                display: block;
+                position: absolute;
+                top: 0;
+                bottom: 0;
+                left: 0;
+                right: 0;
+              }
+            </style>
+          </head>
+          <body>
+            <canvas></canvas>
+            <script>${script.value}</script>
+          </body>
+          </html>
+        ''';
+        break;
+      case 'p5js100':
+      case 'p5js190':
+        html = '''
+          <html>
+          <head>
+            $viewport
+            <meta charset="utf-8">
+            <script>$lib</script>
+            <script>$tokenData</script>
+            <script>${script.value}</script>
+            <style type="text/css">
+              html {
+                height: 100%;
+              }
+              body {
+                min-height: 100%;
+                margin: 0;
+                padding: 0;
+              }
+              canvas {
+                padding: 0;
+                margin: auto;
+                display: block;
+                position: absolute;
+                top: 0;
+                bottom: 0;
+                left: 0;
+                right: 0;
+              }
+            </style>
+          </head>
+          </html>
+        ''';
+        break;
+      case 'paper':
+      case 'twemoji':
+        html = '''
+          <html>
+          <head>
+            $viewport
+            <meta charset="utf-8"/>
+            <script>$lib</script>
+            <script>$tokenData</script>
+            <script>${script.value}</script>
+            <style type="text/css">
+              html {
+                height: 100%;
+              }
+              body {
+                min-height: 100%;
+                margin: 0;
+                padding: 0;
+              }
+              canvas {
+                padding: 0;
+                margin: auto;
+                display: block;
+                position: absolute;
+                top: 0;
+                bottom: 0;
+                left: 0;
+                right: 0;
+              }
+            </style>
+          </head>
+          </html>
+        ''';
+        break;
+      case 'three':
+        html = '''
+          <html>
+            <head>
+              $viewport
+              <script>$lib</script>
+              <meta charset="utf-8">
+              <style type="text/css">
+                body {
+                  margin: 0;
+                  padding: 0;
+                }
+                canvas {
+                  padding: 0;
+                  margin: auto;
+                  display: block;
+                  position: absolute;
+                  top: 0;
+                  bottom: 0;
+                  left: 0;
+                  right: 0;
+                }
+              </style>
+            </head>
+            <body></body>
+            <script>$tokenData</script>
+            <script>${script.value}</script>
+          </html>
+        ''';
+        break;
+      case 'regl':
+        html = '''
+          <html>
+            <head>
+              $viewport
+              <script>$lib</script>
+              <script>$tokenData</script>
+              <meta charset="utf-8">
+              <style type="text/css">
+                body {
+                  margin: 0;
+                  padding: 0;
+                }
+                canvas {
+                  padding: 0;
+                  margin: auto;
+                  display: block;
+                  position: absolute;
+                  top: 0;
+                  bottom: 0;
+                  left: 0;
+                  right: 0;
+                }
+              </style>
+            </head>
+            <body>
+              <script>${script.value}</script>
+            </body>
+          </html>
+        ''';
+        break;
+      case 'tone':
+        html = '''
+          <html>
+            <head>
+              $viewport
+              <script>$lib</script>
+              <meta charset="utf-8">
+              <style type="text/css">
+                body {
+                  margin: 0;
+                  padding: 0;
+                }
+                canvas {
+                  padding: 0;
+                  margin: auto;
+                  display: block;
+                  position: absolute;
+                  top: 0;
+                  bottom: 0;
+                  left: 0;
+                  right: 0;
+                }
+              </style>
+            </head>
+            <body>
+              <canvas></canvas>
+            </body>
+            <script>$tokenData</script>
+            <script>${script.value}</script>
+          </html>
+        ''';
+        break;
+      default:
+        html = '<html><body>hmm</body></html>';
+    }
+    return html;
   }
 
   @override
