26b8ea35e5315e441984d8d79188382343238b0e
ivan grachev <ivan@ivan.lol>
2024-08-26 12:04:08 +0300

    stub webos grid

--- tvs/nft-folder-webos/js/index.js
+++ tvs/nft-folder-webos/js/index.js
@@ -1,3 +1,112 @@
+var eventRegister = (function () {
+  var lastClickedId = null;
+  var itemArray = document.getElementsByClassName("item");
+  var updateText = function (query, value) {
+    var element = document.querySelectorAll(query);
+    if (element && element.length > 0) {
+      element[0].innerText = value;
+    }
+  };
+  var showXYCoordinates = function (details) {
+    updateText("span.x_coor", details.pageX);
+    updateText("span.y_coor", details.pageY);
+  };
+  var _onClickEvent = function (e) {
+    if (lastClickedId) {
+      document.getElementById(lastClickedId).classList.remove("clicked");
+    }
+    document.getElementById(e.target.id).classList.add("clicked");
+    lastClickedId = e.target.id;
+    updateText("p.ok_select", lastClickedId);
+
+    showPlayer(); // TODO: dev tmp. play selected item, not a random one
+  };
+
+  var _onMouseOverEvent = function (e) {
+    for (var i = 0; i < itemArray.length; i++) {
+      _removeFocus(itemArray[i]);
+    }
+    document.getElementById(e.target.id).focus();
+  };
+  var _removeFocus = function (item) {
+    item.blur();
+  };
+  var _itemKeyDownHandler = function (e) {
+    if (e.keyCode === 13) {
+      document.getElementById(e.target.id).classList.add("active");
+    }
+  };
+  var _itemKeyUpHandler = function (e) {
+    if (e.keyCode === 13) {
+      document.getElementById(e.target.id).classList.remove("active");
+      _onClickEvent(e);
+    }
+  };
+  var _itemMouseOutHandler = function (e) {
+    _removeFocus(document.getElementById(e.target.id));
+  };
+  var addEventListeners = function () {
+    window.addEventListener("mouseover", showXYCoordinates);
+    for (var i = 0; i < itemArray.length; i++) {
+      itemArray[i].addEventListener("mouseover", _onMouseOverEvent);
+      itemArray[i].addEventListener("mouseout", _itemMouseOutHandler);
+      itemArray[i].addEventListener("click", _onClickEvent);
+      itemArray[i].addEventListener("keyup", _itemKeyUpHandler);
+      itemArray[i].addEventListener("keydown", _itemKeyDownHandler);
+    }
+  };
+  var cursorVisibilityChange = function (event) {
+    var visibility = event.detail.visibility;
+    if (visibility) {
+      console.log("Cursor appeared");
+    } else {
+      console.log("Cursor disappeared");
+      updateText("span.x_coor", "-");
+      updateText("span.y_coor", "-");
+    }
+  };
+  var keyEventHandler = function (event) {
+    var keyCode = event.keyCode;
+    var type = event.type;
+    var key = event.key || event.keyIdentifier;
+
+    if (key) {
+      updateText("span.key_name", key);
+      updateText("span.key_code", keyCode);
+      updateText("span.key_status", type);
+    }
+    //cursor hide for webOS 1.x
+    if (type === "keydown" && keyCode === 1537) {
+      console.log(type + "  " + keyCode + "  " + key);
+      updateText("span.x_coor", "-");
+      updateText("span.y_coor", "-");
+    }
+  };
+  return {
+    addEventListeners,
+    cursorVisibilityChange,
+    keyEventHandler,
+  };
+})();
+
+window.addEventListener("load", function () {
+  SpatialNavigation.init();
+  SpatialNavigation.add({
+    selector: ".item",
+  });
+  SpatialNavigation.makeFocusable();
+  eventRegister.addEventListeners();
+  document.addEventListener(
+    "cursorStateChange",
+    eventRegister.cursorVisibilityChange,
+    false
+  );
+  document.addEventListener("keydown", eventRegister.keyEventHandler, false);
+  document.addEventListener("keyup", eventRegister.keyEventHandler, false);
+});
+
+// TODO: merge the code above with the code below
+
 document.addEventListener(
   "webOSLaunch",
   function (inData) {
@@ -27,19 +136,17 @@ document.addEventListener(
   visibilityChange,
   function () {
     if (document[hidden]) {
-      removeIframe();
-    } else {
-      createIframe();
+      removePlayer();
     }
   },
   true
 );
 
 function updateIframeContent() {
-  var iframe = document.getElementById('colorFrame');
+  var iframe = document.getElementById("generativePlayerFrame");
   if (iframe) {
-    fetchHtml('sample') // TODO: pass item
-      .then(content => {
+    fetchHtml("sample") // TODO: pass item
+      .then((content) => {
         if (content) {
           iframe.srcdoc = content;
         }
@@ -47,8 +154,8 @@ function updateIframeContent() {
   }
 }
 
-function removeIframe() {
-  var iframe = document.getElementById('colorFrame');
+function removePlayer() {
+  var iframe = document.getElementById("generativePlayerFrame");
   isShowingPlayer = false;
   if (iframe) {
     iframe.remove();
@@ -57,20 +164,20 @@ function removeIframe() {
 
 var isShowingPlayer = false;
 
-function createIframe() {
+function showPlayer() {
   // TODO: if showing player again after it was hidden, make sure to start with the same item
-  if (isShowingPlayer) { return; }
-  if (document[hidden]) { return; }
-  var iframe = document.createElement('iframe');
+  if (isShowingPlayer) {
+    return;
+  }
+  if (document[hidden]) {
+    return;
+  }
+  var iframe = document.createElement("iframe");
   isShowingPlayer = true;
-  iframe.id = 'colorFrame';
+  iframe.id = "generativePlayerFrame";
   document.body.appendChild(iframe);
   updateIframeContent();
 }
-
-window.onload = function() {
-  createIframe();
-};
 
 document.addEventListener(
   "keydown",
@@ -92,95 +199,102 @@ document.addEventListener(
 );
 
 async function fetchHtml(notUsedYetItem) {
-    // TODO: use passed item or stop passing it at all
+  // TODO: use passed item or stop passing it at all
 
-    const itemsJsonString = await readFileContent('assets/items.json');
-    let items;
-    try {
-        items = JSON.parse(itemsJsonString);
-    } catch (error) {
-        return null;
-    }
+  const itemsJsonString = await readFileContent("assets/items.json");
+  let items;
+  try {
+    items = JSON.parse(itemsJsonString);
+  } catch (error) {
+    return null;
+  }
 
-    if (!Array.isArray(items) || items.length === 0) { return null; }
+  if (!Array.isArray(items) || items.length === 0) {
+    return null;
+  }
 
-    const random = Math.floor(Math.random() * items.length);
-    const item = items[random];
+  const random = Math.floor(Math.random() * items.length);
+  const item = items[random];
 
-    let htmlContent;
-    try {
-        htmlContent = await generateHtmlContent(item);
-    } catch (error) {
-        return null;
-    }
+  let htmlContent;
+  try {
+    htmlContent = await generateHtmlContent(item);
+  } catch (error) {
+    return null;
+  }
 
-    return htmlContent.html;
+  return htmlContent.html;
 }
 
 const filePromises = new Map();
 
 function readFileContent(filePath) {
-    if (!filePromises.has(filePath)) {
-        filePromises.set(filePath, loadFile(filePath));
-    }
-    return filePromises.get(filePath);
+  if (!filePromises.has(filePath)) {
+    filePromises.set(filePath, loadFile(filePath));
+  }
+  return filePromises.get(filePath);
 }
 
 function loadFile(filePath) {
-    return new Promise((resolve, reject) => {
-        const iframe = document.createElement('iframe');
-        iframe.style.display = 'none';
-        iframe.src = filePath;
-        document.body.appendChild(iframe);
+  return new Promise((resolve, reject) => {
+    const iframe = document.createElement("iframe");
+    iframe.style.display = "none";
+    iframe.src = filePath;
+    document.body.appendChild(iframe);
 
-        iframe.onload = function() {
-            try {
-                const content = iframe.contentDocument.body.textContent || iframe.contentDocument.body.innerText;
-                resolve(content);
-            } catch (error) {} finally {
-                document.body.removeChild(iframe);
-                filePromises.delete(filePath);
-            }
-        };
+    iframe.onload = function () {
+      try {
+        const content =
+          iframe.contentDocument.body.textContent ||
+          iframe.contentDocument.body.innerText;
+        resolve(content);
+      } catch (error) {
+      } finally {
+        document.body.removeChild(iframe);
+        filePromises.delete(filePath);
+      }
+    };
 
-        iframe.onerror = function() {
-            document.body.removeChild(iframe);
-            filePromises.delete(filePath);
-        };
-    });
+    iframe.onerror = function () {
+      document.body.removeChild(iframe);
+      filePromises.delete(filePath);
+    };
+  });
 }
 
 async function generateHtmlContent(item) {
-    const id = item.address + item.abId;
+  const id = item.address + item.abId;
 
-    const tokensJsonString = await readFileContent(`assets/tokens/${id}.json`);
-    const bundledTokens = JSON.parse(tokensJsonString);
+  const tokensJsonString = await readFileContent(`assets/tokens/${id}.json`);
+  const bundledTokens = JSON.parse(tokensJsonString);
 
-    const scriptJsonString = await readFileContent(`assets/scripts/${id}.json`);
-    const script = JSON.parse(scriptJsonString);
+  const scriptJsonString = await readFileContent(`assets/scripts/${id}.json`);
+  const script = JSON.parse(scriptJsonString);
 
-    const lib = (script.kind === "js" || script.kind === "svg") 
-        ? "" 
-        : await readFileContent(`assets/libs/${script.kind}.js`);
+  const lib =
+    script.kind === "js" || script.kind === "svg"
+      ? ""
+      : await readFileContent(`assets/libs/${script.kind}.js`);
 
-    const random = Math.floor(Math.random() * bundledTokens.items.length);
-    const token = bundledTokens.items[random];
-    const hash = token.hash;
-    const tokenid = token.id;
+  const random = Math.floor(Math.random() * bundledTokens.items.length);
+  const token = bundledTokens.items[random];
+  const hash = token.hash;
+  const tokenid = token.id;
 
-    let tokenData;
-    if (script.address === "0x059edd72cd353df5106d2b9cc5ab83a52287ac3a") {
-        tokenData = `let tokenData = {"tokenId": "${tokenid}", "hashes": ["${hash}"]}`;
-    } else {
-        tokenData = `let tokenData = {"tokenId": "${tokenid}", "hash": "${hash}"}`;
-    }
+  let tokenData;
+  if (script.address === "0x059edd72cd353df5106d2b9cc5ab83a52287ac3a") {
+    tokenData = `let tokenData = {"tokenId": "${tokenid}", "hashes": ["${hash}"]}`;
+  } else {
+    tokenData = `let tokenData = {"tokenId": "${tokenid}", "hash": "${hash}"}`;
+  }
 
-    const viewport = '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>';
+  const viewport =
+    '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>';
 
-    let html;
-    switch (script.kind) {
-        case 'svg':
-            html = `
+  let html;
+  switch (script.kind) {
+    case "svg":
+      html = `
                 <html>
                 <head>
                   ${viewport}
@@ -208,9 +322,9 @@ async function generateHtmlContent(item) {
                 <script>${script.value}</script>
                 </html>
             `;
-            break;
-        case 'js':
-            html = `
+      break;
+    case "js":
+      html = `
                 <html>
                 <head>
                   ${viewport}
@@ -239,10 +353,10 @@ async function generateHtmlContent(item) {
                 </body>
                 </html>
             `;
-            break;
-        case 'p5js100':
-        case 'p5js190':
-            html = `
+      break;
+    case "p5js100":
+    case "p5js190":
+      html = `
                 <html>
                 <head>
                   ${viewport}
@@ -273,10 +387,10 @@ async function generateHtmlContent(item) {
                 </head>
                 </html>
             `;
-            break;
-        case 'paper':
-        case 'twemoji':
-            html = `
+      break;
+    case "paper":
+    case "twemoji":
+      html = `
                 <html>
                 <head>
                   ${viewport}
@@ -307,9 +421,9 @@ async function generateHtmlContent(item) {
                 </head>
                 </html>
             `;
-            break;
-        case 'three':
-            html = `
+      break;
+    case "three":
+      html = `
                 <html>
                 <head>
                   ${viewport}
@@ -337,9 +451,9 @@ async function generateHtmlContent(item) {
                 <script>${script.value}</script>
                 </html>
             `;
-            break;
-        case 'regl':
-            html = `
+      break;
+    case "regl":
+      html = `
                 <html>
                 <head>
                   ${viewport}
@@ -368,9 +482,9 @@ async function generateHtmlContent(item) {
                 </body>
                 </html>
             `;
-            break;
-        case 'tone':
-            html = `
+      break;
+    case "tone":
+      html = `
                 <html>
                 <head>
                   ${viewport}
@@ -400,12 +514,12 @@ async function generateHtmlContent(item) {
                 <script>${script.value}</script>
                 </html>
             `;
-            break;
-        default:
-            html = '<html><body>hmm</body></html>';
-    }
-    return {
-        'html': html,
-        'tokenId': tokenid,
-    };
+      break;
+    default:
+      html = "<html><body>hmm</body></html>";
+  }
+  return {
+    html: html,
+    tokenId: tokenid,
+  };
 }
