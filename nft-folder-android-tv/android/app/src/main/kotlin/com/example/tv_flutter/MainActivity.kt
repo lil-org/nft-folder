48c809b4fed5cd7163a9391bc594f3f5fcff55c2
ivan grachev <ivan@ivan.lol>
2024-10-29 12:41:19 +0300

    recover for android tv

---
+++ nft-folder-android-tv/android/app/src/main/kotlin/com/example/tv_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.lil.nft_folder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ nft-folder-android-tv/lib/src/player/item_repository.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'item.dart';
+import 'dart:math';
+
+class ItemRepository {
+  static final ItemRepository _instance = ItemRepository._internal();
+
+  factory ItemRepository() {
+    return _instance;
+  }
+
+  ItemRepository._internal();
+
+  List<Item> _items = [];
+
+  Future<void> loadItems() async {
+    final String response = await rootBundle.loadString('assets/items/items.json');
+    final List<dynamic> jsonData = json.decode(response);
+    _items = jsonData
+        .map((item) => Item.fromJson(item as Map<String, dynamic>))
+        .where((item) => item.didNotWorkOnProjector != true)
+        .toList();
+  }
+
+  List<Item> get items => _items;
+
+  Item getRandomItem() {
+    if (_items.isEmpty) {
+      throw Exception('No items available');
+    }
+    final random = Random();
+    return _items[random.nextInt(_items.length)];
+  }
+}
