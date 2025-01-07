17d1929a701b37d363a9e1546bc45b89b0c354c2
ivan grachev <ivan@ivan.lol>
2025-01-07 17:23:04 +0300

    reload current html on player size change

--- nft-folder-ios/MobileWebView.swift
+++ nft-folder-ios/MobileWebView.swift
@@ -17,7 +17,7 @@ struct MobileWebView: UIViewRepresentable {
     static func makeNewWebView() -> WKWebView {
         let webConfiguration = WKWebViewConfiguration()
         webConfiguration.suppressesIncrementalRendering = true
-        let wkWebView = WKWebView(frame: .zero, configuration: webConfiguration)
+        let wkWebView = AutoReloadingWebView(frame: .zero, configuration: webConfiguration)
         wkWebView.isOpaque = false
         wkWebView.backgroundColor = .black
         wkWebView.scrollView.backgroundColor = .black
@@ -25,4 +25,45 @@ struct MobileWebView: UIViewRepresentable {
         wkWebView.configuration.userContentController.addUserScript(WKUserScript(source: "document.addEventListener('contextmenu', function(e) { e.preventDefault(); }, false);", injectionTime: .atDocumentEnd, forMainFrameOnly: true))
         return wkWebView
     }
+}
+
+class AutoReloadingWebView: WKWebView {
+    
+    private var lastLoadedHtmlString: String?
+    private var loadedForBounds: CGRect?
+    
+    override var bounds: CGRect {
+        didSet {
+            if bounds != oldValue, let html = lastLoadedHtmlString {
+                _ = loadHTMLString(html, baseURL: nil)
+            }
+        }
+    }
+    
+    override var frame: CGRect {
+        didSet {
+            if frame != oldValue, let html = lastLoadedHtmlString {
+                _ = loadHTMLString(html, baseURL: nil)
+            }
+        }
+    }
+    
+    override func loadHTMLString(_ string: String, baseURL: URL?) -> WKNavigation? {
+        let newHtmlContent = lastLoadedHtmlString != string
+        let newBounds = bounds != loadedForBounds
+        
+        if newHtmlContent {
+            lastLoadedHtmlString = string
+        }
+        
+        loadedForBounds = bounds
+        
+        guard hasVisibleSize, newBounds || newHtmlContent else { return nil }
+        return super.loadHTMLString(string, baseURL: baseURL)
+    }
+    
+    private var hasVisibleSize: Bool {
+        return bounds.size.width > 5 && bounds.size.height > 5
+    }
+    
 }
