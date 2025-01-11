1efc64a6954d9bc8baaeac48e350b80d9449a521
ivan grachev <ivan@ivan.lol>
2025-01-11 15:19:03 +0300

    update email

--- Shared/Extensions/Links.swift
+++ Shared/Extensions/Links.swift
@@ -7,7 +7,7 @@ extension URL {
     static let github = URL(string: "https://github.com/lil-org")!
     static let x = URL(string: "https://x.com/lildotorg")!
     static let warpcast = URL(string: "https://warpcast.com/org")!
-    static let mail = URL(string: "mailto:io@lil.org")!
+    static let mail = URL(string: "mailto:yo@lil.org")!
     static let zora = URL(string: "https://zora.co/collect/zora:0x01c077fd6b4df827490cd4f95650d55d6b35c35d")!
     static let writeAppStoreReview = URL(string: "https://apps.apple.com/app/id6472607192?action=write-review")!
     
