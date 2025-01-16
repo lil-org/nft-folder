379a72c06668275c3007293db49a73b14afd39d2
ivan grachev <ivan@ivan.lol>
2025-01-16 19:00:33 +0300

    add feedback shortcut item

--- Shared/Extensions/Links.swift
+++ Shared/Extensions/Links.swift
@@ -8,6 +8,7 @@ extension URL {
     static let x = URL(string: "https://x.com/lildotorg")!
     static let warpcast = URL(string: "https://warpcast.com/org")!
     static let mail = URL(string: "mailto:yo@lil.org")!
+    static let quickFeedbackMail = URL(string: "mailto:yo@lil.org?subject=Nft%20Folder")!
     static let zora = URL(string: "https://zora.co/collect/zora:0x01c077fd6b4df827490cd4f95650d55d6b35c35d")!
     static let writeAppStoreReview = URL(string: "https://apps.apple.com/app/id6472607192?action=write-review")!
     
--- nft-folder-ios/nft_folder_iosApp.swift
+++ nft-folder-ios/nft_folder_iosApp.swift
@@ -1,12 +1,45 @@
 // ∅ nft-folder 2025
 
 import SwiftUI
+import UIKit
 
 @main
 struct nft_folder_iosApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             MobileCollectionsView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let config = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        config.delegateClass = SceneDelegate.self
+        return config
+    }
+    
+}
+
+private let feedbackShortcutItemType = "org.lil.nft-folder.feedback"
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        if let shortcutItem = connectionOptions.shortcutItem, shortcutItem.type == feedbackShortcutItemType {
+            UIApplication.shared.open(.quickFeedbackMail)
+        }
+    }
+    
+    func windowScene(_ windowScene: UIWindowScene, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        if shortcutItem.type == feedbackShortcutItemType {
+            UIApplication.shared.open(.quickFeedbackMail)
+        }
+        completionHandler(true)
+    }
+    
 }
