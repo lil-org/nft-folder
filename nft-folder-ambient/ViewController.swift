7454f632e020050838bc25688e204a1558f210d6
ivan grachev <ivan@ivan.lol>
2025-01-19 15:52:57 +0300

    add ambient agent target

---
+++ nft-folder-ambient/ViewController.swift
@@ -0,0 +1,21 @@
+// ∅ 2025 lil org
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
