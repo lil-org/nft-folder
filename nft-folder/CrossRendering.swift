0a0693fe437d042aa1811654c3421bd15f91ac89
ivan grachev <ivan@ivan.lol>
2025-01-20 19:23:06 +0300

    refactor cross rendering stub

--- nft-folder/CrossRendering.swift
+++ nft-folder/CrossRendering.swift
@@ -4,43 +4,59 @@ import WebKit
 import QuartzCore
 import IOSurface
 
-var mainWindow: NSWindow!
-var secondWindow: NSWindow!
-var webView: WKWebView!
+class SourceWebViewWindow: NSWindow {
+    var webView: WKWebView!
+    
+    init() {
+        let frame = NSMakeRect(100, 100, 600, 400)
+        super.init(
+            contentRect: frame,
+            styleMask: [.titled, .resizable, .closable],
+            backing: .buffered,
+            defer: false
+        )
+        self.title = "WebView Window"
+        self.makeKeyAndOrderFront(nil)
+        self.contentView?.wantsLayer = true
+        self.contentView?.layer?.backgroundColor = NSColor.gray.cgColor
+        setupWebView()
+    }
+    
+    private func setupWebView() {
+        let config = WKWebViewConfiguration()
+        webView = WKWebView(frame: self.contentView!.bounds, configuration: config)
+        webView.autoresizingMask = [.width, .height]
+        self.contentView?.addSubview(webView)
+        
+        let html = """
+        <html>
+        <body style=\"background:green;\">
+            <div style=\"position:absolute; width:100px; height:100px; background:red; animation:move 2s infinite alternate;\"></div>
+            <style>@keyframes move { from { left: 0px; top: 0px; } to { left: 200px; top: 200px; } }</style>
+        </body>
+        </html>
+        """
+        webView.loadHTMLString(html, baseURL: nil)
+    }
+}
+
+class TargetWindow: NSWindow {
+    init() {
+        let frame = NSMakeRect(750, 100, 600, 400)
+        super.init(
+            contentRect: frame,
+            styleMask: [.titled, .resizable, .closable],
+            backing: .buffered,
+            defer: false
+        )
+        self.title = "Target Render Window"
+        self.makeKeyAndOrderFront(nil)
+        self.contentView?.wantsLayer = true
+        self.contentView?.layer?.backgroundColor = NSColor.gray.cgColor
+    }
+}
 
 func setupWindows() {
-    mainWindow = createWindow(title: "WebView Window", frame: NSMakeRect(100, 100, 600, 400))
-    secondWindow = createWindow(title: "Target Render Window", frame: NSMakeRect(750, 100, 600, 400))
-    setupWebView()
-}
-
-func createWindow(title: String, frame: NSRect) -> NSWindow {
-    let window = NSWindow(
-        contentRect: frame,
-        styleMask: [.titled, .resizable, .closable],
-        backing: .buffered,
-        defer: false
-    )
-    window.title = title
-    window.makeKeyAndOrderFront(nil)
-    window.contentView?.wantsLayer = true
-    window.contentView?.layer?.backgroundColor = NSColor.gray.cgColor
-    return window
-}
-
-func setupWebView() {
-    let config = WKWebViewConfiguration()
-    webView = WKWebView(frame: mainWindow.contentView!.bounds, configuration: config)
-    webView.autoresizingMask = [.width, .height]
-    mainWindow.contentView?.addSubview(webView)
-
-    let html = """
-    <html>
-    <body style="background:green;">
-        <div style="position:absolute; width:100px; height:100px; background:red; animation:move 2s infinite alternate;"></div>
-        <style>@keyframes move { from { left: 0px; top: 0px; } to { left: 200px; top: 200px; } }</style>
-    </body>
-    </html>
-    """
-    webView.loadHTMLString(html, baseURL: nil)
+    let _ = SourceWebViewWindow()
+    let _ = TargetWindow()
 }
