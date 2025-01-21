7752c951db62639d6f701ceaa03a2abfcf298cfa
ivan grachev <ivan@ivan.lol>
2025-01-21 18:42:23 +0300

    fix main app losing focus when starting pip

--- nft-folder/AmbientAgent.swift
+++ nft-folder/AmbientAgent.swift
@@ -6,36 +6,27 @@ struct AmbientAgent {
     
     private init() {}
     
-    // Sender (in main app)
     static func start(generatedToken: GeneratedToken) {
         let helperName = "nft-folder-ambient.app"
         let helperPath = Bundle.main.bundlePath.appending("/Contents/Helpers/\(helperName)")
         let helperURL = URL(fileURLWithPath: helperPath)
 
-        print("yo html length is", generatedToken.html.count)
-        
-        // Launch the agent
-        NSWorkspace.shared.openApplication(at: helperURL, configuration: .init()) { app, error in
-            if let error = error {
-                NSLog("Failed to launch helper: \(error.localizedDescription)")
-                return
-            }
-            // TODO: reduce or remove delay
-            // Give the agent time to set up its notification observer
-            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
-                // Encode token
-                guard let data = try? JSONEncoder().encode(generatedToken),
-                      let jsonString = String(data: data, encoding: .utf8) else {
-                    NSLog("Failed to encode GeneratedToken.")
-                    return
+        if NSWorkspace.shared.runningApplications.contains(where: { $0.bundleIdentifier == "org.lil.nft-folder.ambient" }) {
+            sendTokenNotification(generatedToken: generatedToken)
+        } else {
+            NSWorkspace.shared.openApplication(at: helperURL, configuration: .init()) { _, _ in
+                // TODO: reduce or remove delay
+                // Give the agent time to set up its notification observer
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+                    sendTokenNotification(generatedToken: generatedToken)
                 }
-
-                DistributedNotificationCenter.default().post(
-                    name: Notification.Name("MyTokenNotification"),
-                    object: jsonString // TODO: maybe it's not a good idea to post an entire json
-                )
             }
         }
+    }
+    
+    private static func sendTokenNotification(generatedToken: GeneratedToken) {
+        guard let data = try? JSONEncoder().encode(generatedToken), let jsonString = String(data: data, encoding: .utf8) else { return }
+        DistributedNotificationCenter.default().post(name: Notification.Name("MyTokenNotification"), object: jsonString)
     }
     
 }
