8984f2c5043d4eb0422a465325e02db4bea81dc9
ivan grachev <ivan@ivan.lol>
2025-04-18 23:21:03 +0300

    add external screen placeholder

--- nft-folder-ios/ExternalDisplayViewController.swift
+++ nft-folder-ios/ExternalDisplayViewController.swift
@@ -14,6 +14,7 @@ func updateExternalDisplayToken(_ token: GeneratedToken) {
 class ExternalDisplayViewController: UIViewController {
     
     private var webView: WKWebView!
+    private var placeholderStack: UIStackView!
     private var renderedTokenId = ""
     private var willOrDidAppear = false
     
@@ -27,19 +28,59 @@ class ExternalDisplayViewController: UIViewController {
         fatalError("yo")
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         willOrDidAppear = true
         renderCurrentItem()
     }
     
-    fileprivate func renderToken(_ token: GeneratedToken) {
-        currentToken = token
-        renderCurrentItem()
+    private func ensurePlaceholder() {
+        guard placeholderStack == nil else { return }
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 27
+        let icons = (Bundle.main.infoDictionary?["CFBundleIcons"] as? [String: Any])?["CFBundlePrimaryIcon"] as? [String: Any]
+        let iconFiles = icons?["CFBundleIconFiles"] as? [String]
+        imageView.image = UIImage(named: iconFiles?.last ?? "AppIcon")
+        let label = UILabel()
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Select something in the app."
+        label.font = .preferredFont(forTextStyle: .largeTitle)
+        label.textAlignment = .center
+        label.numberOfLines = 1
+        label.lineBreakMode = .byTruncatingTail
+        
+        placeholderStack = UIStackView(arrangedSubviews: [imageView, label])
+        placeholderStack.translatesAutoresizingMaskIntoConstraints = false
+        placeholderStack.axis = .vertical
+        placeholderStack.spacing = 34
+        placeholderStack.alignment = .center
+        placeholderStack.overrideUserInterfaceStyle = .dark
+        view.addSubview(placeholderStack)
+        
+        NSLayoutConstraint.activate([
+            placeholderStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            placeholderStack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 192),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor),
+            label.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
+        ])
     }
     
     fileprivate func renderCurrentItem() {
         guard willOrDidAppear else { return }
+        
+        let isEmpty = currentToken.html.isEmpty
+        ensurePlaceholder()
         
         if webView == nil {
             webView = AutoReloadingWebView.new
@@ -53,6 +94,9 @@ class ExternalDisplayViewController: UIViewController {
                 webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
             ])
         }
+        
+        placeholderStack.isHidden = !isEmpty
+        webView.isHidden = isEmpty
         
         if currentToken.id == renderedTokenId {
             return
